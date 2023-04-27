8a20dc7692b562f4fa5a42781577856078b07f68
zNahuelz <102327040+zNahuelz@users.noreply.github.com>
2023-04-27 17:35:17 -0500

    Initial commit

---
+++ View/frmLogin.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace LongDrinkSys_Local
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblLostContr = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LongDrinkSys_Local.Properties.Resources.logo_longdrink_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(129, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(219, 191);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(186, 24);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "INICIO DE SESIÓN";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(172, 230);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(68, 16);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "USUARIO";
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContrasena.Location = new System.Drawing.Point(139, 263);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(101, 16);
+            this.lblContrasena.TabIndex = 3;
+            this.lblContrasena.Text = "CONTRASEÑA";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(266, 228);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(203, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(266, 261);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '*';
+            this.txtContrasena.Size = new System.Drawing.Size(203, 20);
+            this.txtContrasena.TabIndex = 5;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(243, 328);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(139, 23);
+            this.btnIniciar.TabIndex = 6;
+            this.btnIniciar.Text = "INICIAR SESIÓN";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(243, 357);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(139, 23);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblLostContr
+            // 
+            this.lblLostContr.AutoSize = true;
+            this.lblLostContr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLostContr.ForeColor = System.Drawing.Color.Red;
+            this.lblLostContr.Location = new System.Drawing.Point(208, 296);
+            this.lblLostContr.Name = "lblLostContr";
+            this.lblLostContr.Size = new System.Drawing.Size(208, 16);
+            this.lblLostContr.TabIndex = 8;
+            this.lblLostContr.Text = "¿Olvidó su contraseña? Click aquí";
+            this.lblLostContr.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // frmLogin
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(625, 410);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblLostContr);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblContrasena);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LONG DRINK - INICIO DE SESIÓN";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblLostContr;
+    }
+}
+
---
+++ View/frmLogin.cs
@@ -0,0 +1,69 @@
+﻿using LongDrinkSys_Local.Model;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LongDrinkSys_Local
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult pregunta = MessageBox.Show("¿Está seguro que desea salir?", "CONSULTA", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (pregunta == DialogResult.Yes) { Application.Exit(); }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text.Length > 0 && txtContrasena.Text.Length > 0)
+            {
+                bool Login = IniciarSesion(txtUsuario.Text, txtContrasena.Text);
+                if (Login)
+                {
+                    MessageBox.Show("Login correcto!");
+                    //TO-DO: Permisos, form principal...
+                }
+                else
+                {
+                    MessageBox.Show("Credenciales incorrectas!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe completar ambos campos!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        public bool IniciarSesion(string usr, string pass)
+        {
+            bool r = false;
+            try
+            {
+                using (LongDrinkEntities db = new LongDrinkEntities())
+                {
+                    var sql = db.Usuario.FirstOrDefault(e => e.nombre_usuario == usr && e.contrasena == pass);
+                    if (sql != null)
+                    {
+                        r = true;
+                    }
+                    else { r = false; }
+                }
+            }
+            catch (Exception)
+            {
+                r = false;
+            }
+            return r;
+        }
+    }
+}
